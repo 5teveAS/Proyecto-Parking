7173031d92f36f407a419fcf39c65fdf03d2ea60
Steve AS <stevarcsa@gmail.com>
2021-07-06 02:32:40 -0400

    version1

---
+++ js/sidenav.js
@@ -0,0 +1,35 @@
+function openNav() {
+
+  if (innerWidth < 630) {
+
+    document.getElementById("mySidenav").style.width = "100%";
+    
+    document.getElementById("main").style.marginRight = "100%";
+
+  } else {
+    
+    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("main").style.marginRight = "350px";
+    
+  }
+
+  document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
+}
+
+
+const prueba = document.querySelector('#main');
+
+document.onclick = function (e) {
+  if (e.target.id !== 'mySidenav' &&
+    e.target.id !== 'openSideMenu' &&
+    e.target.id !== 'nombreUsuario' &&
+    e.target.id !== 'form') {
+    closeNav();
+  }
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+  document.body.style.backgroundColor = "white";
+}
