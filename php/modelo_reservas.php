e019db90dadfe53153255e793373f7ed49f7bd11
FabricioRC69 <78940151+FabricioRC69@users.noreply.github.com>
2021-08-08 00:23:08 -0600

    Ingreso de datos al espacio de parqueo completo

--- php/modelo_reservas.php
+++ php/modelo_reservas.php
@@ -47,20 +47,55 @@ $conn =  mysqli_connect('localhost', 'root', '', 'parqueo');
     </div>
     <div class="tablaDeIngresoDatos">
         <form>
-            <input type="text" name="ced_id">
-            <input type="text" name="placa">
-            <input type="text" name="nombre">
-            <input type="text" name="apellido1">
-            <input type="text" name="telefono">
-            <input type="datetime" name="horaEntrada">
-            <input type="datetime" name="horaSalida">
-            <input type="number" name="id_lugar">
-            <input type="submit" name="" value="Ingresar">
-            <a href="index.html">Regresar</a>
+            <label>Cedula</label><br>
+            <input type="text" name="ced_id"><br><br>
+            <label>Placa</label><br>
+            <input type="text" name="placa"><br><br>
+            <label>Nombre</label><br>
+            <input type="text" name="nombre"><br><br>
+            <label>Apellido</label><br>
+            <input type="text" name="apellido1"><br><br>
+            <label>Telefono</label><br>
+            <input type="text" name="telefono"><br><br>
+            <label>Hora de entrada</label><br>
+            <input type="datetime-local" name="horaEntrada"><br><br>
+            <label>Hora de salida</label><br>
+            <input type="datetime-local" name="horaSalida"><br><br>
+            <label>Numero de lugar</label><br>
+            <input type="number" name="id_lugar"><br><br>
+            <input type="submit" name="ingresar" value="Ingresar"><br><br>
+            <a href="../index.html">Regresar</a>
 
         </form>
 
     </div>
+    <?php
+
+    $consultaEstado = "SELECT estado from estacionamiento where id_lugar='" . $id_lugar . "';";
+    if (isset($_GET['ingresar'])) {
+        $ced = $_GET['ced_id'];
+        $placa = $_GET['placa'];
+        $nombre = $_GET['nombre'];
+        $apellido1 = $_GET['apellido1'];
+        $telefono = $_GET['telefono'];
+        $horaEntrada = $_GET['horaEntrada'];
+        $horaSalida = $_GET['horaSalida'];
+        $id_lugar = $_GET['id_lugar'];
+        $nuevoEstado = "Ocupado";
+
+        $resultadoQ = mysqli_query($conn, $consultaEstado);
+        if ($ced != null || $placa != null || $nombre != null) {
+            $Reserva = "INSERT INTO infotiquet(ced_id,placa,nombre,apellido1,telefono,horaEntrada,horaSalida,id_lugar) 
+        VALUES('" . $ced . "','" . $placa . "','" . $nombre . "','" . $apellido1 . "','" . $telefono . "','" . $horaEntrada . "','" . $horaSalida . "','" . $id_lugar . "')";
+            mysqli_query($conn, $Reserva);
+            $updateEstacionamiento = "UPDATE estacionamiento
+        SET estado = '" . $nuevoEstado . "'
+        WHERE id_lugar = '" . $id_lugar . "';";
+            mysqli_query($conn, $updateEstacionamiento);
+        }
+    }
+
+    ?>
 
 </body>
 
